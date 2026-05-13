62e2b612c9c42cbf6069aa3ae1a2a8071f3dc40e
phmfe <pedrohmfe11@gmail.com>
2026-05-13 20:06:04 -0300

    Update test_ex5.py

--- test_ex5.py
+++ test_ex5.py
@@ -1,12 +1,12 @@
 def calcular_bonus(salario_base: float, avaliacao: str) -> float:
-    # Regra de segurança para salário negativo
+   
     if salario_base < 0:
         return 0.0
     
-    # Padronizando a string (Primeira letra maiúscula)
+  
     avaliacao = avaliacao.capitalize()
     
-    # Definindo o percentual do bônus
+
     if avaliacao == "Excelente":
         percentual = 0.20
     elif avaliacao == "Bom":
@@ -16,11 +16,11 @@ def calcular_bonus(salario_base: float, avaliacao: str) -> float:
     else:
         percentual = 0.0  # Para "Ruim" ou qualquer outra coisa
         
-    # Calculando o valor final
+
     valor_final = salario_base * percentual
     return valor_final
 
-# --- Execução dos Testes ---
+
 print(f"Bônus Excelente: R$ {calcular_bonus(3000, 'Excelente')}")
 print(f"Bônus Bom: R$ {calcular_bonus(2000, 'bom')}")
 print(f"Bônus Regular: R$ {calcular_bonus(1000, 'REGULAR')}")
